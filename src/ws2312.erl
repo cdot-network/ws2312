ee6cb4ca45bcd3ee7d7fd3cc1961e5874bf2ef9f
sprhawk <465558+sprhawk@users.noreply.github.com>
2022-07-20 04:08:08 +0000

    added gen_server behavior

--- src/ws2312.erl
+++ src/ws2312.erl
@@ -1,17 +1,28 @@
 -module(ws2312).
+-behavior(gen_server).
 
 -export([start_link/0]).
 
--export([init/1]).
+-export([
+         init/1,
+         handle_call/3,
+         handle_cast/2
+        ]).
+
+-export([
+         blink/2
+        ]).
+
+-type led_handle() :: pid() | undefined.
 
 -record(state, {
-    handle = pid() :: undefined
+    handle :: led_handle()
 }).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-init() ->
+init([]) ->
     %% Trap exits to allow for terminate led animation
     erlang:process_flag(trap_exit, true),
 
@@ -20,7 +31,26 @@ init() ->
             State = #state {
                        handle = Handle
                       },
-            State;
+            {ok, State};
         _ ->
-            undefined
+            {err}
     end.
+
+blink(Pid, State) ->
+    gen_server:cast(Pid, blink, State).
+
+handle_call(Msg, _From, State) ->
+    lager:warning("Unhandled call ~p: ~p", [Msg, State]),
+    {noreply, State}.
+
+handle_cast(blink, State = #state{handle = undefined}) ->
+    lager:info("blinking with undefined"),
+    {noreply, State};
+handle_cast(blink, State) ->
+    spi:transfer(State#state.handle),
+    {noreply, State};
+handle_cast(Msg, State) ->
+    lager:warning("Unhandled cast ~p: ~p", [Msg, State]),
+    {noreply, State}.
+
+    
